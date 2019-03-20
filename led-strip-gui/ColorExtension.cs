a2dd3c8feaf496aa3cd9ae72616dd1f98d0568dc
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2019-03-20 17:32:55 +0000

    adding helper functions for manipulating color

---
+++ led-strip-gui/ColorExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace LedStripGui
+{
+    public static class ColorExtension
+    {
+        public static byte GetByteHue(this Color c)
+        {
+            var hue = c.GetHue();
+            // hue range from 0 to 360
+            var b = (byte)((int)(hue / 360f * 255f));
+
+            return b;
+        }
+        public static byte GetByteSaturation(this Color c, float saturationIncrease = 1f)
+        {
+            var colorSat = c.GetSaturation();
+            // increase saturation by halving the distance to 1
+            var oneMinusSat = 1 - colorSat;
+            var realSat = 1 - (oneMinusSat / saturationIncrease);
+
+            // sat range from 0 to 1
+            var b = (byte)((int)(realSat * 255f));
+
+            return b;
+        }
+        public static byte GetByteValue(this Color c)
+        {
+            var val = c.GetBrightness();
+            // val range from 0 to 1
+            var b = (byte)((int)(val * 255f));
+
+            return b;
+        }
+    }
+}
